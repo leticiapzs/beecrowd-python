69742dedb005092535ce1628f3fef7b0c8afbee8
leticiapzs <leticia_95lima@hotmail.com>
2024-09-14 10:06:19 -0300

    Extremamente básico

---
+++ iniciante/ExtremamenteBásico.py
@@ -0,0 +1,5 @@
+"""Leia 2 valores inteiros e armazene-os nas variáveis A e B. Efetue a soma de A e B atribuindo o seu resultado na variável X."""
+a= int(input())
+b = int(input())
+x = a+b
+print("X =", x)
